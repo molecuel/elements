b3a1c38a0bf3cc29ae9daff173d1256ef64cf541
Dominic Böttger <dominic.boettger@inspirationlabs.com>
2017-09-07 18:32:13 +0200

    Update MlclElements.ts

--- lib/classes/MlclElements.ts
+++ lib/classes/MlclElements.ts
@@ -3,7 +3,7 @@ import {MlclConfig, MlclCore} from "@molecuel/core";
 import {MlclDatabase} from "@molecuel/database";
 import {di, injectable} from "@molecuel/di";
 import * as TSV from "@molecuel/tsvalidate";
-import * as Jsonpatch from "fast-json-patch";
+import {compare, applyPatch} from "fast-json-patch";
 import * as _ from "lodash";
 import "reflect-metadata";
 import {DiffObject} from "./DiffObject";
@@ -177,12 +177,12 @@ export class MlclElements {
   }
 
   public diffObjects(oldObj, newObj) {
-    const diff: DiffObject[] = Jsonpatch.compare(newObj, oldObj);
+    const diff: DiffObject[] = compare(newObj, oldObj);
     return diff;
   }
 
   public revertObject(obj, patches: DiffObject[]) {
-    const result = Jsonpatch.apply(obj, patches);
+    const result = applyPatch(obj, patches);
     return result;
   }
 
